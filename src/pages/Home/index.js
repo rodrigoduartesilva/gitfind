870f9e1a9fee41d4f22da6eb807e358e462e5b7b
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-17 17:51:46 -0300

    feat: input e botao de buscas de respositorios implementado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,30 @@
-import { Header } from "../../components/Header";
+import { Header } from '../../components/Header';
+import background from '../../assets/img/background.png';
+import './styles.css';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='content'>
+        <img src={background} className='content__backgroundImg' alt='Background App' />
+        <div className='content__info'>
+          <div>
+            <input name='usuario' placeholder='@username' />
+            <button>Buscar</button>
+          </div>
+          <div className='content__profile'>
+            <img src='https://avatars.githubusercontent.com/u/47402277?v=4' className='content__profile__img' alt='Rodrigo Duarte' />
+            <div>
+              <h3>Rodrigo Duarte</h3>
+              <span>@rodrigoduartesilva</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
     </div>
   );
 }
